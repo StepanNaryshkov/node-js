52e649a0b2e1618e1f96b4a78c121ff2773a9ac7
StepanNaryshkov <stepan.naryshkov@eleks.com>
2021-05-31 16:31:49 +0300

    added server

--- app.js
+++ app.js
@@ -1,46 +1,5 @@
 const http = require('http');
-const fs = require('fs');
-
-
-function handleServer(req, res) {
-    const { url, method } = req;
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Example</title></head>');
-        res.write('<body><h1>Test</h1><a href="/form">form</a></body>')
-        res.write('</html>');
-        res.end();
-    } else {
-        res.write(`<!DOCTYPE html>
-        <html lang="en">
-        <head>
-          <meta charset="UTF-8">
-          <meta http-equiv="X-UA-Compatible" content="IE=edge">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Document</title>
-        </head>
-        <body>
-          <form action="/message" method="POST"><input type="text" name="text"></form>
-        </body>
-        </html>`);
-        res.end();
-    }
-
-    if (url === '/message' && 'POST') {
-        const body = [];
-        
-        req.on('data', (chunk) => {
-            console.log('chunk', chunk)
-            body.push(chunk)
-        });
-
-        req.on('end', () => {
-            const parseBody = Buffer.concat(body).toString();
-            console.log('parseBody12', parseBody)
-            fs.writeFileSync('message.txt',  parseBody.split('=')[1])
-        })
-    }
-}
+const handleServer = require('./route');
 
 const server = http.createServer(handleServer);
 
---
+++ route.js
@@ -0,0 +1,63 @@
+const users = ['lol'];
+
+function handleServer(req, res) {
+  const { url, method } = req;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Example</title></head>');
+    res.write('<body><h1>Test</h1><a href="/create-user">form</a></body>')
+    res.write('</html>');
+    res.end();
+  } else if (url === '/create-user' && method === 'GET'){
+    res.write(`<!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Document</title>
+        </head>
+        <body>
+          <form action="/create-user" method="POST"><input type="text" name="username"></form>
+          <a href="/users">users</a>
+        </body>
+        </html>`);
+    return res.end();
+  } else if (url === '/users'){
+    res.write(`<!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Document</title>
+        </head>
+        <body>
+        <ul>`);
+    users.map(user => res.write(`<li>${user}</li>`))
+    res.write(`               
+        </ul>
+        </body>
+        </html>`)
+    res.end();
+  }
+console.log('url', url)
+console.log('method', method)
+  if (url === '/create-user' && method === 'POST') {
+    const body = [];
+
+    req.on('data', (chunk) => {
+      body.push(chunk)
+    });
+
+    req.on('end', () => {
+      const parseBody = Buffer.concat(body).toString();
+      users.push(parseBody.split('=')[1]);
+    })
+    res.statusCode = 302;
+    res.setHeader('Location', '/users');
+    res.end();
+  }
+}
+
+module.exports = handleServer;
